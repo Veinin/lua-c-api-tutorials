6bdf7d91f52e4aa8daa27cb40aa7ce9a0f20a897
DownToTwo <yourf1493@gmail.com>
2023-03-30 13:06:50 +0300

    some fixes

--- src/09_sleep_lib.c
+++ src/09_sleep_lib.c
@@ -1,26 +1,23 @@
 #include <unistd.h>
 
-#include "lua.h"
 #include "lauxlib.h"
+#include "lua.h"
 #include "lualib.h"
 
-static int s_sleep(lua_State *L)
-{
-    long seconds = lua_tointeger(L, -1);
-    sleep(seconds);
-    return 0;
+static int s_sleep(lua_State *L) {
+  long seconds = lua_tointeger(L, -1);
+  sleep(seconds);
+  return 0;
 }
 
-static int u_sleep(lua_State *L)
-{
-    long usec = lua_tointeger(L, -1);
-    usleep(usec);
-    return 0;
+static int m_sleep(lua_State *L) {
+  long usec = lua_tointeger(L, -1);
+  usleep(usec);
+  return 0;
 }
 
-int luaopen_msleep(lua_State *L)
-{
-    lua_register(L, "sleep", s_sleep);
-    lua_register(L, "msleep", m_sleep);
-    return 0;
+int luaopen_msleep(lua_State *L) {
+  // lua_register(L, "sleep", s_sleep);
+  lua_register(L, "msleep", m_sleep);
+  return 0;
 }
